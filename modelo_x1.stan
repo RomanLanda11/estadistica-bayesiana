66b8eea09e38f432a2380fcf66cd462f447d26aa
RomanLanda <roman.landa@gruposancristobal.com>
2024-06-19 12:30:06 -0300

    cominezo modelado

---
+++ modelo_x1.stan
@@ -0,0 +1,13 @@
+data {
+  int<lower=1> N;             // Cantidad de observaciones
+  int<lower=0, upper=1> y[N]; // Vector de respuesta (0 y 1)
+  vector[N] x;
+}
+parameters {
+  real a;
+  real b;
+}
+model {
+  // Notar que 'a' y 'b' reciben priors uniformes
+  y ~ bernoulli_logit(a + b*x);
+}
--- tp_final/src/auto_eda.R
+++ tp_final/src/auto_eda.R
@@ -1,16 +1,11 @@
 library(readr)
-df <- read_csv("data/cerebros.csv")
+df <- read_csv("tp_final/data/cerebros.csv")
 df$diag <- factor(df$diag, levels = c("HC", "MCI", "AD"))
 levels(df$diag)
 
 #install.packages("DataExplorer")
 library(DataExplorer)
 create_report(df)
-
-
-#install.packages("SmartEDA")
-library(SmartEDA)
-ExpReport(df)
 
 
 # Instalar y cargar la librería
